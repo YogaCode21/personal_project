0a8b85558d843b74d0eee62371a80d611f76deef
YogaCode21 <yogacode2006@gmail.com>
2023-11-16 21:17:21 +0800

    change tailwind color to base color

--- pomolife/app/components/ModalInput.jsx
+++ pomolife/app/components/ModalInput.jsx
@@ -5,11 +5,11 @@ const ModalInput = ({ label, defaultValue, onChange, name }) => {
     <div className="form-group flex flex-col flex-auto">
       <label
         htmlFor={name}
-        className="text-xs font-bold text-pmd-blue-300 pb-1">
+        className="text-xs font-bold text-gray-600 pb-1">
         {label}
       </label>
       <input
-        className="settingsInput w-24 bg-pmd-blue-50 py-2 px-4 text-sm font-bold rounded-xl focus:outline-none"
+        className="settingsInput w-24 bg-[#262626] text-white py-2 px-4 text-sm font-bold rounded-xl focus:outline-none"
         min="1"
         max="60"
         type="number"
